b51fef71ab977892acf7c80b0adb95bf7ad29b12
Patrick Enburg <patrick@enburg.dev>
2024-09-01 11:56:40 -0700

    Added a jsonStatus method to the statusable interface

--- statusable.h
+++ statusable.h
@@ -2,15 +2,21 @@
 #define STATUSABLE_H
 
 #include <QObject>
+#include <QJsonDocument>
 
 class Statusable
 {
 
 public:
     virtual QString getStatus() const;
+    virtual QJsonDocument jsonStatus() const = 0;
 
 signals:
 
+protected:
+    inline static const QString sKeyName = "Name";
+    inline static const QString sKeyValue = "Value";
+    inline static const QString sKeyDesc = "Desc";
 };
 
 #endif // STATUSABLE_H
